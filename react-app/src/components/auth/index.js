ab9006365352ebe25fdbfb8c8567452ed6769f8d
Ryan Webster <ryan.h.webster@gmail.com>
2021-02-11 11:36:43 -0800

    set form modals

---
+++ react-app/src/components/auth/index.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Modal } from '../../context/Modal';
+import LoginForm from './LoginForm';
+import SignupForm from './SignUpForm';
+
+function LoginFormModal() {
+    const [showModal, setShowModal] = useState(false);
+    const [formType, setFormType] = useState('login');
+
+    return (
+        <>
+            <button onClick={() => {
+                setShowModal(true)
+                setFormType('login')}}>Log In</button>
+            <button onClick={() => {
+                setShowModal(true)
+                setFormType('signUp')}}>Sign Up</button>
+            {showModal && (
+            <Modal onClose={() => setShowModal(false)}>
+                {(formType === 'login') && <LoginForm />}
+                {(formType === 'signUp') && <SignupForm />}
+            </Modal>
+            )}
+        </>
+    )
+}
