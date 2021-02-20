8461745a3ef899d155fa66ae82884f7d4b917a7d
Ryan Webster <ryan.h.webster@gmail.com>
2021-02-19 22:03:26 -0800

    final adjustment tonight

--- migrations/versions/20210219_220300_.py
+++ migrations/versions/20210219_220300_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: a1094f06cdf3
+Revision ID: 16fdb459e88c
 Revises: 
-Create Date: 2021-02-19 21:54:07.468112
+Create Date: 2021-02-19 22:03:00.330008
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'a1094f06cdf3'
+revision = '16fdb459e88c'
 down_revision = None
 branch_labels = None
 depends_on = None
