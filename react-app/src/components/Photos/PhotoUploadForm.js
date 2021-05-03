8994ae21bc7b7ac09aa9b551d308706f14337719
Ryan Webster <ryan.h.webster@gmail.com>
2021-05-03 16:05:20 -0700

    adding tagging a stadium to photos

--- react-app/src/components/Photos/PhotoUploadForm.js
+++ react-app/src/components/Photos/PhotoUploadForm.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { getStadiums, userStadiums } from '../../store/stadium';
 import { addPhoto, addSubmission } from '../../store/photos';
 import './photo.css';
 
