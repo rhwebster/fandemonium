3199043c3c2821d4a95b73f7fce688d745c51dc2
Ryan Webster <ryan.h.webster@gmail.com>
2021-08-10 13:32:26 -0400

    fixing redux persist

--- react-app/src/store/configureStore.js
+++ react-app/src/store/configureStore.js
@@ -1,12 +1,14 @@
 import { createStore } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
-import rootReducer from './reducers'
+
+import rootReducer from './index'
 
 const persistConfig = {
     key: 'root',
     storage,
 }
+
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
