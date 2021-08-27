38574e56a63b93f84af35049bf4f3ed2635bc04b
Ryan Webster <ryan.h.webster@gmail.com>
2021-08-27 14:21:15 -0700

    working on store

--- react-app/src/store/configureStore.js
+++ react-app/src/store/configureStore.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { createStore } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import storage from 'redux-persist/lib/storage'
 
 import rootReducer from './index'
 
