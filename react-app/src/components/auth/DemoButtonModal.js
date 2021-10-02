45d5d36c8a8f00527320ade41cbbf8a3c3a871c7
Ryan Webster <ryan.h.webster@gmail.com>
2021-10-02 10:56:34 -0700

    demo login

--- react-app/src/components/auth/DemoButtonModal.js
+++ react-app/src/components/auth/DemoButtonModal.js
@@ -1,19 +1,29 @@
 import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import DemoForm from './DemoForm';
+import * as sessionActions from "../../store/session";
+
 
 const DemoButtonModal = () => {
     const [showModal, setShowModal] = useState(false);
 
+    const onLogin = async (e) => {
+        e.preventDefault();
+        setErrors([]);
+        await dispatch(sessionActions.login({ 'demo@aa.io', 'password' }))
+        history.push('/')
+    };
+
     return (
         <>
-            <button id='demoButton' onClick={() =>
+            {/* <button id='demoButton' onClick={() =>
                 setShowModal(true)}>Demo</button>
             {showModal && (
                 <Modal onClose={() => setShowModal(false)} name='demo'>
                     <DemoForm />
                 </Modal>
-            )}
+            )} */}
+            <button id='demoButton' onClick={onLogin}>Demo</button>
         </>
     )
 }
